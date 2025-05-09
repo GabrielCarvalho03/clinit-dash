29a0116b468fad5a1624158855a14da6228c9990
GabrielCarvalho03 <gabrielcarvalho1734@gmail.com>
2025-05-08 21:02:13 -0300

    frist

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -11,5 +11,17 @@ declare namespace NodeJS {
     NEXT_PUBLIC_AWS_REGION: string;
     NEXT_PUBLIC_AWS_ACCESS_KEY: string;
     NEXT_PUBLIC_AWS_SECRET_KEY: string;
+
+    FIREBASE_TYPE: string;
+    FIREBASE_PROJECT_ID: string;
+    FIREBASE_PRIVATE_KEY_ID: string;
+    FIREBASE_PRIVATE_KEY: string;
+    FIREBASE_CLIENT_EMAIL: string;
+    FIREBASE_CLIENT_ID: string;
+    FIREBASE_AUTH_URI: string;
+    FIREBASE_TOKEN_URI: string;
+    FIREBASE_AUTH_PROVIDER_X509_CERT_URL: string;
+    FIREBASE_CLIENT_X509_CERT_URL: string;
+    FIREBASE_UNIVERSE_DOMAIN: string;
   }
 }
